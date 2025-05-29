c53d779df216c7af44b2ff571a8c3111af3fe9c5
Ana Luísa <116834271+naluisaelias@users.noreply.github.com>
2025-05-29 16:03:53 -0300

    Aula 10 (parte 02) - Slide 16

---
+++ aula10/TelaXViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TelaXViewController.swift
+//  aula10
+//
+//  Created by Ana Luísa on 29/05/25.
+//
+
+import UIKit
+
+class TelaXViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
