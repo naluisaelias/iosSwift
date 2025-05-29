97a6753ffe3cdb1d872be67ef5f731744cb55110
Ana Luísa <116834271+naluisaelias@users.noreply.github.com>
2025-05-29 14:12:50 -0300

    Aula 10 - Navigation Controller

---
+++ aula10/aula10/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  aula10
+//
+//  Created by Ana Luísa on 29/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func fechar(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+}
+
